4d2f48933e72f628c5629fa7b4c02e6d115fc8c5
Tango_Charlie <loulou.reyms@orange.fr>
2022-12-15 11:22:51 +0100

    EXIT_SUCCES

--- fichiers_struc/SAE_anuaire.c
+++ fichiers_struc/SAE_anuaire.c
@@ -75,7 +75,7 @@ int main(int argc, char **argv)
 
 			default:
 				printf("\n");
-				printf("Veuillez selectionner numéro correct\n");
+				printf("Veuillez sélectionner numéro correct\n");
 		}
 	}
 
